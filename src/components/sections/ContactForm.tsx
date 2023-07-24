c6b1975a8b783934afd999ee393409d970613f76
Dania "Gord1y" Gordienko <95370285+Gord1y@users.noreply.github.com>
2023-07-25 02:10:24 +0300

    fix

--- src/components/sections/ContactForm.tsx
+++ src/components/sections/ContactForm.tsx
@@ -117,13 +117,13 @@ export default function ContactForm() {
 			<div className='text-xl md:text-2xl w-full'>Будь які додаткові дані</div>
 			<textarea
 				name='message'
-				rows={5}
+				rows={3}
 				placeholder='Повідомлення'
 				className='w-full p-2 rounded-md border-2 border-black placeholder:text-secondary outline-none focus:border-black'
 			/>
 			<button
 				type='submit'
-				className='w-full md:w-1/2 p-2 rounded-md border-2 border-black text-black hover:bg-black hover:text-white'
+				className='mt-1 w-full md:w-1/2 p-2 rounded-md border-2 border-black text-black hover:bg-black hover:text-white'
 			>
 				Відправити
 			</button>
--- src/components/sections/Prices.tsx
+++ src/components/sections/Prices.tsx
@@ -8,24 +8,30 @@ export default function App() {
 			<div className='flex flex-col md:flex-row overflow-hidden justify-center items-center w-full mt-5 gap-5 md:gap-8'>
 				<div className='flex flex-col gap-2 justify-center items-center p-5 snap-center border-2 border-secondary text-secondary rounded-lg md:w-1/4'>
 					<p className='text-3xl md:text-4xl'>Пробне заняття</p>
-					<p className='text-2xl md:text-3xl'>250грн/60хв</p>
-					<p className='text-xl md:text-2xl'>
+					<p className='text-3xl md:text-4xl text-rose-500 font-black'>
+						250грн/60хв
+					</p>
+					<p className='text-2xl md:text-3xl'>
 						1 пробне заняття, на якому оцінюється рівень знань, та ми розуміємо
 						чи підходимо один одному
 					</p>
 				</div>
-				<div className='flex flex-col gap-2 justify-center items-center p-10 snap-center bg-secondary rounded-lg text-primary md:w-1/4'>
+				<div className='flex flex-col gap-2 justify-center items-center p-10 snap-center bg-secondary rounded-lg text-primary md:w-1/3'>
 					<p className='text-3xl md:text-4xl'>Блок занять</p>
-					<p className='text-2xl md:text-3xl'>2999грн/11 занять</p>
-					<p className='text-xl md:text-2xl'>
+					<p className='text-3xl md:text-4xl text-rose-500 font-black'>
+						2999грн/11 занять
+					</p>
+					<p className='text-2xl md:text-3xl'>
 						Найвигідніший тариф, оскільки є додаткове заняття по ціні 10
 						разових.
 					</p>
 				</div>
 				<div className='flex flex-col gap-2 justify-center items-center p-5 snap-center border-2 border-black text-black rounded-lg md:w-1/4'>
 					<p className='text-3xl md:text-4xl'>Разове заняття</p>
-					<p className='text-2xl md:text-3xl'>300грн/60хв</p>
-					<p className='text-xl md:text-2xl'>
+					<p className='text-3xl md:text-4xl text-rose-400 font-black'>
+						300грн/60хв
+					</p>
+					<p className='text-2xl md:text-3xl'>
 						Наступний урок, після пробного, під час якого уже починаємо щось
 						розбирати та обираємо шляхи вивчення
 					</p>
